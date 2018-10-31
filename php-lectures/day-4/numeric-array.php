0f07c07e15e4cf93ca9f28278e15beeb7ebb348d
Archy2212 <40794153+Archy2212@users.noreply.github.com>
2018-10-31 23:25:07 +0530

    Update numeric-array.php

--- php-lectures/day-4/numeric-array.php
+++ php-lectures/day-4/numeric-array.php
@@ -12,7 +12,7 @@
          /* Second method to create array. */
          $numbers[0] = " one ";
          $numbers[1] = " two ";
-         $numbers[2] = "three ";
+         $numbers[2] = " three ";
          $numbers[3] = "four ";
          $numbers[4] = "five ";
          
