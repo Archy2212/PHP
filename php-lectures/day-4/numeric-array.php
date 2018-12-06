56a22ef6e329b1b0130022264b484065c8611051
Archy2212 <40794153+Archy2212@users.noreply.github.com>
2018-12-06 23:42:42 +0530

    Update numeric-array.php

--- php-lectures/day-4/numeric-array.php
+++ php-lectures/day-4/numeric-array.php
@@ -2,7 +2,7 @@
    <body>
 
       <?php
-         /* First method to create array. */
+         /* First method to create array. *
          $numbers = array( 1, 2, 3, 4, 5);
          
          foreach( $numbers as $value ) {
